230e446e0760c8875068502b3843007806062eca
Daniel Vasile <daniel.vasile@campfire.ch>
2020-04-07 13:28:41 +0200

    added files for authors creation

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 
+//All authors route
 router.get('/', (req, res) => {
-	res.render('index');
+	res.render('authors/index');
+});
+
+// New Author Route
+router.get('/new',(req,res)=>{
+	res.render('authors/new');
+});
+
+// Create Author Route
+router.post('/', (req,res)=>{
+	res.send('Create');
 });
 
 module.exports = router;
