08d980af994054365d0433e8f3fff235b4e0146b
Daniel Vasile <daniel.vasile@campfire.ch>
2020-04-07 13:52:07 +0200

    Ok

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,9 @@ const router  = express.Router();
 
 //All authors route
 router.get('/', (req, res) => {
-	res.render('authors/index');
+	res.render('index');
 });
 
-// New Author Route
-router.get('/new',(req,res)=>{
-	res.render('authors/new');
-});
-
-// Create Author Route
-router.post('/', (req,res)=>{
-	res.send('Create');
-});
 
 module.exports = router;
 // Hello
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ if(process.env.NODE_ENV !== 'production'){
 const express        = require('express');
 const app            = express();
 const expressLayouts = require('express-ejs-layouts');
+
 const indexRouter    = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -23,6 +25,7 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log("Connected to mongoose"));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 5000);
 
